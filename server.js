3ae2d99d950aeb20b34b33dc4f8aa451ab8c9665
jnnh <jenniferhorvath@cmail.carleton.ca>
2022-02-13 17:38:47 -0500

    first route pre-test

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const { notes } = require('./Develop/db/db');
+
+app.get('/api/notes', (req, res)=>{
+    res.json(notes);
+});
+
+app.listen(PORT, ()=> {
+    console.log(`API server now on port ${PORT}!`);
+});
+
