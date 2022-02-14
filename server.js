e0626a0a2f2f7693080397122e90b876322f0455
jnnh <jenniferhorvath@cmail.carleton.ca>
2022-02-13 23:06:41 -0500

    middlewares added

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const path = require('path');
 
 const { createNewNote, validateNote } = require('./Develop/lib/notes');
 const { notes } = require('./Develop/db/db');
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
 
 //api routes
 app.get('/api/notes', (req, res)=>{
